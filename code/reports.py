0bcaf3361df3f8e58e3c20a5606499a7f99bddcd
Traci Kamp <traci.kamp@gmail.com>
2018-12-14 17:38:14 -0600

    Add query to get 3 most popular articles

--- code/reports.py
+++ code/reports.py
@@ -1,1 +1,27 @@
 #!/usr/bin/env python
+import psycopg2
+
+DBNAME = 'news'
+
+MOST_POP_3_ART = """
+select articles.title, count(articles.title) as views
+from articles, log
+where log.path like concat('%',articles.slug,'%')
+and log.status = '200 OK'
+group by articles.title
+order by views desc
+limit 3;
+"""
+
+
+def get_three_most_pop():
+    print "\n1. What are the most popular three articles of all time?"
+    with psycopg2.connect(dbname=DBNAME) as db:
+        cursor = db.cursor()
+        cursor.execute(MOST_POP_3_ART)
+        for title, views in cursor.fetchall():
+            print '"' + title + '"', '--', views, "views"
+
+
+if __name__ == "__main__":
+    get_three_most_pop()
